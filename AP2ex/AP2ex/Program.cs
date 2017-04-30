73142b45ab32c8a001764384e6f64dae6debbff6
DanielMualem <dani.mualem@gmail.com>
2017-04-30 16:52:14 +0300

    Add files via upload

---
+++ AP2ex/AP2ex/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AP2ex
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("dani");
+            Console.WriteLine("4343");
+        }
+    }
+}
